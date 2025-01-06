9be05dc9a746ca5bb55b283a8e64be4260bdbea7
Zhu Tianwen <ztw1002@126.com>
2025-01-06 21:24:12 +0800

    C9 done

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -11,12 +11,6 @@ import { formatCurrency } from './utils'
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`
 
     return data.rows

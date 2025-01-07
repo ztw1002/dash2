b1a46fe4213ddb55dc1fa163ceb12676f1fb2fde
Zhu Tianwen <ztw1002@126.com>
2025-01-07 19:01:50 +0800

    something wrong with C12

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -1,24 +1,25 @@
-'use client';
-
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+'use client'
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
+} from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { Button } from '@/app/ui/button'
+import { updateInvoice } from '@/app/lib/actions'
 
 export default function EditInvoiceForm({
   invoice,
   customers,
 }: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
+  invoice: InvoiceForm
+  customers: CustomerField[]
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -119,5 +120,5 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  );
+  )
 }

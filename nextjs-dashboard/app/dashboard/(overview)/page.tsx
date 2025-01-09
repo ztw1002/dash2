26e6936fdc4eb6622bc2c9dac381cad90fa81b89
Zhu Tianwen <ztw1002@126.com>
2025-01-09 18:07:21 +0800

    delete the eslint error

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -5,7 +5,6 @@ import { lusitana } from '@/app/ui/fonts'
 import { Suspense } from 'react'
 import {
   RevenueChartSkeleton,
-  LatestInvoicesSkeleton,
 	CardsSkeleton
 } from '@/app/ui/skeletons'
 
